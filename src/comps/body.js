5c10cf4a162d30f4bcdb9be98c5d25810cf711c7
sam <ponnusamykrish23@gmail.com>
2023-02-16 07:21:16 +0530

    "Initial commit"

---
+++ src/comps/body.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import styles from '../css/body.module.css'
+function Body() {
+
+//  const handleClick = ()=>{
+//         localStorage.setItem("firstName", "william")
+//         console.log(localStorage.getItem("firstName"))
+//     }
+
+const Notes = () =>{
+    return(
+        <div className={styles.pm}>
+           hello
+        </div>
+    )
+}
+
+
+  return (
+    <div>
+        
+<Notes />
+
+
+
+
+    </div>
+  )
+}
+
+
+// handleClick = ()=>{
+//     console.log("clicked")
+// }
+
+
+
+
+export default Body
---
+++ src/comps/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../css/header.module.css'
+
+function Header() {
+  return (
+    <div className={styles.head}>
+        <p className={styles.pm}>Notes App</p>
+    </div>
+  )
+}
+
+export default Header
